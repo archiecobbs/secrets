1377e619ba63c7e2e782c289775027b6982d2b0e
Archie L. Cobbs <archie.cobbs@gmail.com>
2020-02-18 16:28:08 -0600

    Add usage message to helper scripts.

--- initialize-secrets.sh
+++ initialize-secrets.sh
@@ -3,31 +3,84 @@
 # Bail out on error
 set -e
 
-# Verify we are root
-if [ "`id -u`" -ne 0 ]; then
-    echo "${0}: you must run this as root" 1>&2
-    exit 1
-fi
-
-# Variables
+# Constants
 MIN_SIZE="3"
 CRYPTSETUP="/sbin/cryptsetup"
-CRYPTFILE="./filesystem.bin"
+DEFAULT_CRYPTFILE="filesystem.bin"
+if [ -n "${SUDO_USER}" ]; then
+    DEFAULT_DEVNAME="secrets_${SUDO_USER}"
+else
+    DEFAULT_DEVNAME="secrets_${USER}"
+fi
+
+# Get device name
 if [ -n "${SUDO_USER}" ]; then
     DEVNAME="secrets_${SUDO_USER}"
 else
     DEVNAME="secrets_${USER}"
 fi
 
-# Get size
-if [ $# -eq 1 ] && [[ "${1}" =~ [0-9]+ ]]; then
-    SIZE="${1}"
-else
-    echo "Usage: ${0} size-in-megabytes" 1>&2
+# Usage message
+usage()
+{
+    echo "Usage: initialize-secrets.sh [options] size-in-megabytes" 1>&2
+    echo "Options:" 1>&2
+    echo "    -d devname    Specify device name (default \"${DEFAULT_DEVNAME}\")" 1>&2
+    echo "    -f file       Specify image file (default \"${DEFAULT_CRYPTFILE}\")" 1>&2
+    echo "    -h,--help     Show this help mesage" 1>&2
+}
+
+# Parse flags passed in on the command line
+DEVNAME="${DEFAULT_DEVNAME}"
+CRYPTFILE="${DEFAULT_CRYPTFILE}"
+while [ ${#} -gt 0 ]; do
+    case "$1" in
+        -d)
+            shift
+            DEVNAME="${1}"
+            shift
+            ;;
+        -f)
+            shift
+            CRYPTFILE="${1}"
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+case "${#}" in
+    1)
+        SIZE="$1"
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
+# Sanity check size
+if ! [[ "${1}" =~ [0-9]+ ]]; then
+    echo "${0}: invalid size \"${1}\"" 1>&2
     exit 1
 fi
 if [ "${SIZE}" -lt "${MIN_SIZE}" ]; then
     echo "${0}: size must be at least ${MIN_SIZE}" 1>&2
+    exit 1
+fi
+
+# Verify we are root
+if [ "`id -u`" -ne 0 ]; then
+    echo "${0}: you must run this as root" 1>&2
     exit 1
 fi
 
--- mount-secrets.sh
+++ mount-secrets.sh
@@ -3,26 +3,81 @@
 # Bail out on error
 set -e
 
+# Constants
+CRYPTSETUP="/sbin/cryptsetup"
+DEFAULT_CRYPTFILE="filesystem.bin"
+DEFAULT_MNTDIR="secrets.mount"
+if [ -n "${SUDO_USER}" ]; then
+    DEFAULT_DEVNAME="secrets_${SUDO_USER}"
+else
+    DEFAULT_DEVNAME="secrets_${USER}"
+fi
+
+# Usage message
+usage()
+{
+    echo "Usage: mount-secrets.sh [options]" 1>&2
+    echo "Options:" 1>&2
+    echo "    -d devname    Specify device name (default \"${DEFAULT_DEVNAME}\")" 1>&2
+    echo "    -f file       Specify image file (default \"${DEFAULT_CRYPTFILE}\")" 1>&2
+    echo "    -m dir        Specify mount directory (default \"${DEFAULT_MNTDIR}\")" 1>&2
+    echo "    -h,--help     Show this help mesage" 1>&2
+}
+
+# Parse flags passed in on the command line
+DEVNAME="${DEFAULT_DEVNAME}"
+CRYPTFILE="${DEFAULT_CRYPTFILE}"
+MNTDIR="${DEFAULT_MNTDIR}"
+while [ ${#} -gt 0 ]; do
+    case "$1" in
+        -d)
+            shift
+            DEVNAME="${1}"
+            shift
+            ;;
+        -f)
+            shift
+            CRYPTFILE="${1}"
+            shift
+            ;;
+        -m)
+            shift
+            MNTDIR="${1}"
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+case "${#}" in
+    0)
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
 # Verify we are root
 if [ "`id -u`" -ne 0 ]; then
     echo "${0}: you must run this as root" 1>&2
     exit 1
 fi
 
-# Variables
-CRYPTSETUP="/sbin/cryptsetup"
-CRYPTFILE="./filesystem.bin"
-MNTDIR="./secrets.mount"
-if [ -n "${SUDO_USER}" ]; then
-    DEVNAME="secrets_${SUDO_USER}"
-else
-    DEVNAME="secrets_${USER}"
-fi
-
 # Open encrypted device
 "${CRYPTSETUP}" --type luks open "${CRYPTFILE}" "${DEVNAME}"
 
 # Mount device
+mkdir -p "${MNTDIR}"
 mount -o noatime,nodiratime /dev/mapper/"${DEVNAME}" "${MNTDIR}"
 
 # Done
